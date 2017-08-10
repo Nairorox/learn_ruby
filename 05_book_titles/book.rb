d699e7d8e297138616274d6a8dc0264b3c21e755
Nairorox <Xororian@o2.pl>
2017-08-10 21:10:53 +0200

    05 done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    returning_title = ""
+    first_word_index = 0
+        split_title = @title.split(" ")
+        split_title.each do |title_part|
+          if (title_part == "and"  or title_part == "in" or title_part =="the" or title_part == "of" or title_part == "a" or title_part == "an") and first_word_index != 0
+            returning_title += title_part + " "
+          else
+      returning_title += title_part.capitalize + " "
+    end
+    first_word_index += 1
+  end
+    returning_title[-1, 1] = ""
+    return returning_title
+  end
 end
